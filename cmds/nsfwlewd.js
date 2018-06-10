f375ce7fc764f471a5a93114c9bc3400f853139b
SirNightmare <38385282+SirNightmare@users.noreply.github.com>
2018-06-10 02:27:15 +0100

    Update nsfwlewd.js

--- cmds/nsfwlewd.js
+++ cmds/nsfwlewd.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const superagent = require("superagent");
 const send = require("quick.hook");
-var prefix = "¬"
+var prefix = "~"
 
 module.exports.run = async (bot, message, args) => {
 
