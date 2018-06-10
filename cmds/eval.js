7f079f0c42a8bce050433180ca5f872f0f5a25d0
SirNightmare <38385282+SirNightmare@users.noreply.github.com>
2018-06-10 02:23:43 +0100

    Update eval.js

--- cmds/eval.js
+++ cmds/eval.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const prefix = "¬"
+const prefix = "~"
 const fs = require("fs");
 let bot = new Discord.Client();
 module.exports.run = async (bot, message, args) => {
